87bd3dd0e97557ef6fa6a87939dbedd237a246bc
Asad Asad <43581992+AsadThafer@users.noreply.github.com>
2022-12-12 14:18:50 +0200

    Task 3
    Calling Add with a negative number will throw an exception “negatives not allowed” - and the negative that was passed. if there are multiple negatives, show all of them in the exception message.

--- String_calculator.js
+++ String_calculator.js
@@ -1,12 +1,19 @@
+const Negatives = [];
 
 function String_calculator(numbers) {
   if (numbers.length === 0) return 0;
   const nums = numbers.split(',');
   const answer = nums.reduce((acc, value) => {
     let val = parseInt(value);
+    if (val < 0) {
+      Negatives.push(val);
+      return acc
+    }
     return acc + val;
   }, 0)
-    return answer;
+  if (Negatives.length) throw new Error('negatives not allowed ' + Negatives.join(',') ) 
+  return answer;
 }
+
 
 module.exports = String_calculator;
--- String_calculator.test.js
+++ String_calculator.test.js
@@ -2,14 +2,18 @@ const String_calculator = require("./String_calculator");
 
 describe("StringCalculator's test cases", () => {
   test("addition of empty String to be 0 ", () => {
-    expect(String_calculator('')).toBe(0);
+    expect(String_calculator("")).toBe(0);
   });
 
   test("addition of one numbers to equal the same number", () => {
-    expect(String_calculator('7')).toBe(7);
+    expect(String_calculator("7")).toBe(7);
   });
 
   test("addition of two numbers to equal the sum", () => {
-    expect(String_calculator('100, 50')).toBe(150);
+    expect(String_calculator("100, 50")).toBe(150);
+  });
+  test("addition of negative numbers to give error", () => {
+    const f=()=>{String_calculator("-100,-50")}
+    expect(f).toThrow( "negatives not allowed -100,-50");
   });
 });
